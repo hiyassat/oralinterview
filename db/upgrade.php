52ce3b480fb36038d77bb249a1c02ccc2d0fe428
HusamALSaket <alsaket.husam@gmail.com>
2026-02-16 12:32:10 +0300

    Oral Interview

--- db/upgrade.php
+++
@@ -1,6 +0,0 @@
-<?php
-function xmldb_oralinterview_upgrade($oldversion) {
-    global $DB;
-    $dbman = $DB->get_manager();
-    return true;
-}
--- version.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->component = 'mod_oralinterview';
-$plugin->version = 2026010100;
-$plugin->requires = 2024042200;
-$plugin->maturity = MATURITY_ALPHA;
-$plugin->release = 'v0.1 (Alpha)';
-$plugin->dependencies = [];
